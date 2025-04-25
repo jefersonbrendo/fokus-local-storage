ca51965be32c1ec3eadd07ce3ac18018084fb2c3
jefersonbrendo <jefersonbrendo077@gmail.com>
2025-04-25 00:09:31 -0300

    disparar broadcast para elementos

--- script-crud.js
+++ script-crud.js
@@ -8,6 +8,7 @@ const paragrafoDescricaoTarefa = document.querySelector(
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -61,10 +62,12 @@ function criarElementoTarefa(tarefa) {
     if (tarefaSelecionada == tarefa) {
       paragrafoDescricaoTarefa.textContent = "";
       tarefaSelecionada == null;
+      liTarefaSelecionada = null;
       return;
     }
 
     tarefaSelecionada = tarefa;
+    liTarefaSelecionada = li;
     paragrafoDescricaoTarefa.textContent = tarefa.descricao;
 
     li.classList.add("app__section-task-list-item-active");
@@ -94,3 +97,11 @@ tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
   listaTarefas.append(elementoTarefa);
 });
+
+document.addEventListener("focoFinalizado", () => {
+  if (tarefaSelecionada && liTarefaSelecionada) {
+    liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
+    liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+    liTarefaSelecionada.querySelector("button").setAttribute("disabled", "disabled");
+  }
+})
